c7faa1ff718d380f1f38f58983aa2b72434185bd
rozhnov <lrozhnov@neoflex.ru>
2024-12-16 08:41:02 +0300

    Initial commit

---
+++ src/main/java/nflx/bench/CreditPipeline/CreditPipelineApplication.java
@@ -0,0 +1,13 @@
+package nflx.bench.CreditPipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditPipelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreditPipelineApplication.class, args);
+	}
+
+}
